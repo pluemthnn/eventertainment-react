06dc6567abd0cff5d7b803a315c41eeb37a3b102
gdoyssaga <51605556+gdoyssaga@users.noreply.github.com>
2021-05-01 18:27:43 +0700

    IMG size is not fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,29 +12,37 @@ const Button = styled.button`
   &:hover {
     background-color: white;
     border: 1px solid black;
+    color: black;
   }
 `;
 const MyRow = styled.div`
-  margin-top: 100px;
+  margin-top: 30px;
   background-color: rgb(255, 231, 234);
   border-radius: 30px;
   box-shadow: 12px 12px 22px rgba(0, 0, 0, 0.1);
 `;
-const HomePage = () => {
+const HomePage = (props) => {
   return (
-    <div className="container">
-      <h1>Homepage</h1>
-      <MyRow>
-        <h2>All event</h2>
-        <img src="https://www.flaticon.com/svg/vstatic/svg/1636/1636028.svg?token=exp=1619852446~hmac=a8a356ed90f97dc04c8652a733c8db4c" />
+    <body className="my-3">
+      <div className="container">
+        <h1 className="mt-4">Welcom to Admin Homepage</h1>
+        <MyRow>
+          <h2>All event</h2>
+          <img src="https://www.flaticon.com/svg/vstatic/svg/1636/1636028.svg?token=exp=1619852446~hmac=a8a356ed90f97dc04c8652a733c8db4c" />
 
-        <p>Let's manage our fucking event</p>
-        <Button type="button" style={{}}>
-          {" "}
-          Manage event
-        </Button>
-      </MyRow>
-    </div>
+          <h5 className="py-3">Let's manage our lovely event</h5>
+          <Button
+            type="button"
+            onClick={() => {
+              props.history.push("/EventMng");
+            }}
+          >
+            {" "}
+            Manage event
+          </Button>
+        </MyRow>
+      </div>
+    </body>
   );
 };
 
