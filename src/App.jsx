488b633433da778d683246d3bf60a1d116f33837
gdoyssaga <51605556+gdoyssaga@users.noreply.github.com>
2021-05-01 14:28:51 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import Navbar from "./components/header";
 import Footerbar from "./components/footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 
-import Home from './pages/Home'
-import UserMng from './pages/UserMng'
-import EventMng from './pages/EventMng'
-import Search from './pages/Search'
-import Login from './pages/Login'
-
+import Home from "./pages/Home";
+import UserMng from "./pages/UserMng";
+import EventMng from "./pages/EventMng";
+import Search from "./pages/Search";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route exact path='/' component={Home}/>
+        <Route exact path="/" component={Home} />
         {/* <Route exact path='/aboutus' component={AboutUs}/> */}
         {/* <Route exact path='/search' component={Search}/> */}
-        <Route exact path='/UserMng' component={UserMng}/>
-        <Route exact path='/EventMng' component={EventMng}/>
-        <Route exact path='/Login' component={Login}/>
+        <Route exact path="/UserMng" component={UserMng} />
+        <Route exact path="/EventMng" component={EventMng} />
+        <Route exact path="/Login" component={Login} />
       </Switch>
       <Footerbar />
-      
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 const Button = styled.button`
-border: none;
+  border: none;
   outline: none;
   height: 50px;
   width: 20%;
@@ -8,28 +8,34 @@ border: none;
   color: white;
   border-radius: 4px;
   font-weight: bold;
+  margin-bottom: 40px;
+  &:hover {
+    background-color: white;
+    border: 1px solid black;
+  }
 `;
-const box = styled.div`
+const MyRow = styled.div`
+  margin-top: 100px;
   background-color: rgb(255, 231, 234);
   border-radius: 30px;
   box-shadow: 12px 12px 22px rgba(0, 0, 0, 0.1);
-
 `;
 const HomePage = () => {
   return (
+    <div className="container">
+      <h1>Homepage</h1>
+      <MyRow>
+        <h2>All event</h2>
+        <img src="https://www.flaticon.com/svg/vstatic/svg/1636/1636028.svg?token=exp=1619852446~hmac=a8a356ed90f97dc04c8652a733c8db4c" />
 
-    <div>
-      <box>
-      Homepage
-      <h2>All event</h2>
-      <p>Let's manage our fucking event</p>
-      <Button type="button" style={{}}> Manage event</Button>
-    </box>
+        <p>Let's manage our fucking event</p>
+        <Button type="button" style={{}}>
+          {" "}
+          Manage event
+        </Button>
+      </MyRow>
     </div>
-    
-  )
-}
+  );
+};
 
-
-
-export default HomePage
+export default HomePage;
